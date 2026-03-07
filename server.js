151da2798ba01dcdfd65f4ac70a38cfbeefcaaf2
Fevzi <helyora349@gmail.com>
2026-03-07 17:15:34 +0300

    router fix

--- server.js
+++ server.js
@@ -2420,7 +2420,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
