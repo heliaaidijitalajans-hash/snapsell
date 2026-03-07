3f60dd261d32bed7cc705d5806328f2ae471e86b
Fevzi <helyora349@gmail.com>
2026-03-07 16:33:03 +0300

    static fix

--- server.js
+++ server.js
@@ -2416,6 +2416,8 @@ function startServer(port) {
   });
 }
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
