a84aad02f9aad959bf26bd908f33e2c898e8a15b
Fevzi <helyora349@gmail.com>
2026-03-14 23:29:04 +0300

    kesin cozum: axios ve port ayarları

--- server.js
+++ server.js
@@ -3068,4 +3068,9 @@ app.use(function (err, req, res, next) {
   res.status(500).json({ error: err.message || "Server error" });
 });
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server is running on port", PORT);
+});
+
 module.exports = { app };
